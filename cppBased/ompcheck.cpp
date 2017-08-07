9dd2dcc8b1541393bb9143ab4da0808ecc85530e
OSUmageed <mageed@oregonstate.edu>
2017-08-06 19:56:38 -0700

    Revamped mpi_omp to get the actual omp thread number, time, and cpuid

--- cppBased/ompcheck.cpp
+++ cppBased/ompcheck.cpp
@@ -1,7 +1,21 @@
 #include <unordered_map>
+#include <iostream>
 #include <cstdio>
-#include <stdlib.h>
+#include <cstdlib>
+#include <string>
+#include <unistd.h>
 #include <omp.h>
+#include <sched.h>
+
+#ifdef _WIN32
+  #define ENVP(x) _putenv(x)
+#elif __linux__
+  #define ENVP(x) putenv(x)
+#else
+  #define ENVP(x) _putenv(x)
+#endif
+
+// g++ -o ../bin/ompchk(.exe) ompcheck.cpp -lm -fopenmp -std=c++11
 
 using namespace std;
 
@@ -12,10 +26,26 @@ int main(int argc, char *argv[])
   printf("We have OpenMP %s.\n", map.at(_OPENMP).c_str());
   int threadLimit = omp_get_thread_limit();
   printf("Number of possible threads on this processor: %d.\n", threadLimit);
-  int procLimit = omp_get_num_procs();
-	char ompenv[]="OMP_DISPLAY_ENV=VERBOSE";
-	putenv(ompenv);
+  int procLimit = omp_get_num_procs(); 
+  char ompenv[]="OMP_PLACES=sockets(1)";
+  putenv(ompenv);
+
   printf("Number of available processors: %i.\n", procLimit);
+
+  int nt = atoi(argv[1]); 
+
+  cout << "Threads requested: " << nt << endl;
+
+  #pragma omp parallel for num_threads(nt)
+  for (int n=0; n<nt; n++)
+  {
+    double tm = omp_get_wtime();
+    int tid = omp_get_thread_num();
+    int cid = sched_getcpu();
+    printf("Thread: %i -- on CPU: %i --- Time %.9f.\n", tid, cid, tm);
+    sleep(5);
+    fflush(stdout); 
+  }
 
   return 0;
 }
--- mpiBased/mpi_omp.cpp
+++ mpiBased/mpi_omp.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-// #include <typeinfo>
 #include <mpi.h>
 #include <omp.h>
-
+#include <sched.h>
+#include <unistd.h>
 
 static int ranks, rlen=800;
+
+// mpirun -np 2 ../mpiBased/bin/mpi_omp 32
 
 using namespace std;
 
@@ -16,12 +18,24 @@ void getDeviceInformation()
     char procN[rlen];
     int nthreads = omp_get_num_procs();
     MPI_Get_processor_name(procN,  &rlen);
+    int cid = sched_getcpu();
 
     cout << ranks << " " << procN << " " << nthreads << endl;
 
-    // From this I want what GPUs each proc can see, and how many threads they can make
-    // This may require nvml to get the UUID of the GPUS, pass them all up to the 
-    // Master proc to decide which proc gets which gpu.
+}
+
+void threadLocTest(int nt)
+{
+    #pragma omp parallel for num_threads(nt)
+    for (int n=0; n<nt; n++)
+    {
+        double tm = omp_get_wtime();
+        int tid = omp_get_thread_num();
+        int cid = sched_getcpu();
+        printf("Rank: %i --- Thread: %i --- on CPU: %i --- Time %.9f.\n", ranks, tid, cid, tm);
+        sleep(5);
+        fflush(stdout); 
+    }
 }
 
 // Test device sight.
@@ -32,6 +46,13 @@ int main(int argc, char *argv[])
     if (!ranks) cout << "rank -- cpuname -- nThreads" << endl;
     MPI_Barrier(MPI_COMM_WORLD);
     getDeviceInformation();
+    MPI_Barrier(MPI_COMM_WORLD);
+    cout << endl;
+
+    if (!ranks) cout << "rank -- tid -- cpuid -- Time" << endl;
+    // Look at where the threads are place in OMP when running with two+ procs
+    int nt = atoi(argv[1]); //Nthreads
+    threadLocTest(nt);
     MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Finalize();
 }
